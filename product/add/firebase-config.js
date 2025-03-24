e1a238e06be6c4926000dbc936db727fa60e5c7b
Daffa Aditya <79897029+daffadevhosting@users.noreply.github.com>
2025-03-24 15:42:52 +0700

    Update firebase-config.js

--- product/add/firebase-config.js
+++ product/add/firebase-config.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA3ra4E7chQhRzERyKW2aFUTRULMb10vxM",
+  authDomain: "blogdaffadev.firebaseapp.com",
+  databaseURL: "https://blogdaffadev-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "blogdaffadev",
+  storageBucket: "blogdaffadev.firebasestorage.app",
+  messagingSenderId: "489703180131",
+  appId: "1:489703180131:web:4383f5ef04c1d414ce3556",
+  measurementId: "G-ZWW4K4NVQ6"
 };
 
 // Inisialisasi Firebase
