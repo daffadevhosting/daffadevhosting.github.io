5ae4ab501861bfd1e7bd8d2f72d762685c200337
Daffa Aditya <79897029+daffadevhosting@users.noreply.github.com>
2025-03-28 23:08:06 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const cleanCss = require('gulp-clean-css')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('assets/sass/**/*.scss')
+    return src('_sass/**/*.scss')
         .pipe(sass({
             includePaths: ['node_modules'],
          }).on('error', sass.logError))
