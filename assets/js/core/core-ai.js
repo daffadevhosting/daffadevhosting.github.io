06f5bb795ef66c00ec3054142cf66a85af138cec
daffadevhosting <endsae83@gmail.com>
2025-07-20 10:34:45 +0700

    update

--- assets/js/core/core-ai.js
+++ assets/js/core/core-ai.js
@@ -348,7 +348,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   const matrixArray = matrix.split("");
 
-  const fontSize = 12;
+  const fontSize = 14;
   const columns = canvas.width / fontSize;
   const drops = [];
 
@@ -361,15 +361,15 @@ function drawMatrix() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = '#00ffff';
-  ctx.shadowColor = '#08b4e9ff';
-  ctx.shadowBlur = 5; // Kekuatan efek cahaya
+  ctx.shadowColor = '#00b9f1ff';
+  ctx.shadowBlur = 15; // Kekuatan efek cahaya
   ctx.font = fontSize + 'px monospace';
 
     for (let i = 0; i < drops.length; i++) {
   const text = matrixArray[Math.floor(Math.random() * matrixArray.length)];
   ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
-    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.957) {
     drops[i] = 0;
   }
     drops[i]++;
